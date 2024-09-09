0305a3e1b643f62615b99f8a98eb21ea150949f9
maxuying maxuying <maxuying@jinlab-svr2.icb.ac.cn>
2024-09-09 22:28:55 +0800

    YOUR COMMIT

---
+++ 1.prepare_count_matrix/1.cellranger.sh
@@ -0,0 +1,9 @@
+id=$1
+fq_dir=$2
+ref_dir=$3 ##cellranger reference genome files
+fq_id=$4 ## fq data name
+
+### after changing the fastq data name to cellranger format
+
+cellranger count --id=$id --transcriptome=${ref_dir} --fastqs=${fq_dir} --sample=${fq_id}
+
